e7bc7a46b6395cb81dfe2a229b7cf45999884cbd
Obrienjames34 <Obrienjames934@gmail.com>
2022-04-25 20:21:13 -0400

    student lenght

--- 02-Iteration/Unsolved/script.js
+++ 02-Iteration/Unsolved/script.js
@@ -1,3 +1,7 @@
 // TODO: Write Your JavaScript Code Here
 var students = ["Isreal", "James", "Philip", "Marie", "Michael"];
-console.log(students);
+
+console.log(students.lenght)
+
+for(var i = 0; i < students.Length; i++) {
+}
